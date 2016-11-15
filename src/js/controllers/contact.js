967b2160dc3f548fda7e8849244fdb10b84e1145
Jack Storrs <jackstorrs8@gmail.com>
2016-11-15 16:49:32 -0500

    added required fields and a method to check that all fields are properly formatted before storing the form data.

--- src/js/controllers/contact.js
+++ src/js/controllers/contact.js
@@ -4,6 +4,7 @@ function ContactController ($scope, $http) {
   $scope.contacts = [];
   $scope.errors = {};
 
+
   function init() {
     $http.get(SERVER_URL).then(function (response) {
       console.log(response);
@@ -14,17 +15,65 @@ function ContactController ($scope, $http) {
   init();
 
   $scope.addContact = function (contact) {
-    $http.post(SERVER_URL, contact).then(function (response) {
-      let contact = response.data;
-      $scope.contacts.push(contact);
-      console.log($scope.contacts);
-      console.log(contact)
-      $scope.contact = {}
-    })
-  }
+      if ($scope.validateForm(contact)) {
+        $http.post(SERVER_URL, contact).then(function (response) {
+        let contact = response.data;
+        $scope.contacts.push(contact);
+        console.log($scope.contacts);
+        console.log(contact)
+        $scope.contact = {}
+        });
+      } ;
+  };
 
   $scope.validateName = function (name) {
-    
+    if (name === '') {
+      $scope.errors.name = "Must supply a name.";
+      return false;
+    } else {
+      $scope.errors.name = ''
+      return true;
+    };
+  };
+
+  $scope.validateUrl = function (url) {
+      if (url && (url === '' || !url.startsWith('http'))) {
+        $scope.errors.website = "Must supply a valid URL starting with http:// or https://";
+        return false;
+      } else {
+        $scope.errors.website = '';
+        return true;
+      };
+  };
+
+  $scope.validateEmail = function (email) {
+    if (email && !email.includes('@')) {
+      $scope.errors.email = 'Must supply a valid email address. (www.example@email.com)';
+      return false;
+    } else {
+      $scope.errors.email = '';
+      return true;
+    };
+  };
+
+  $scope.validateMessage = function (message) {
+    if (message === '') {
+      $scope.errors.message = 'Message cannot be empty.';
+      return false;
+    } else {
+      $scope.errors.message = '';
+      return true;
+    };
+  };
+
+
+  $scope.validateForm = function (contact) {
+    console.log(contact)
+    if ($scope.validateMessage(contact.message) && $scope.validateEmail(contact.email) && $scope.validateUrl(contact.website) && $scope.validateName(contact.name)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   ContactController.$inject = [$scope, $http];
